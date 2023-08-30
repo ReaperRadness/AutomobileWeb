2b6e32acc4a08dcd9fb62eb44949df66273fe5ba
ReaperRadness <98930919+ReaperRadness@users.noreply.github.com>
2023-08-30 20:52:58 +0700

    Add files via upload

---
+++ src/Components/Header.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let cars = ["Supercars", "Hypercars", "Roadsters", "Volchrons"];
+</script>
+
+<header>
+  <div class="grid">
+    {#each cars as car}
+      <div class="car">{{ car }}</div>
+    {/each}
+  </div>
+</header>
+
+<style>
+  header {
+  }
+
+  .grid {
+    display: grid;
+    grid-template-areas: "cars";
+    grid-template-columns: 3fr repeat(4, 1fr);
+    grid-template-rows: 1fr;
+    align-items: center;
+    justify-items: center;
+  }
+
+  .car {
+    border: 1px solid black;
+    padding: 1rem;
+  }
+</style>
